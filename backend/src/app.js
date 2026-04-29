b3e856b80fd5141f65ef40f96d62e9b82f70ed1d
emvmontiel <emvmontiel@gmail.com>
2026-04-29 17:47:09 +0800

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express(); // Create an express app
 
+app.use(express.json());
+
+// Import routes
+import userRouter from './routes/user.route.js';
+
+// Route declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,10 +2,13 @@
 // This is where paths are handled
 
 import { Router } from 'express'
+import { registerUser } from '../controllers/user.controller';
 
 // Router is a tool that helps us get routes in express
 // Express is the framework which handles the routes; router is the tool we are going to use
 
 const router = Router();
+
+router.post('/register').post(registerUser);
 
 export default router;
